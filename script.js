24e7cc0a86feb863f61bba00237582e35d587e0f
Luis Baião <luisfbaiao@hotmail.com>
2023-10-07 20:15:34 +0100

    finished?

--- script.js
+++ script.js
@@ -28,40 +28,40 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function isInputValid(playerSelection) {
-    if (playerSelection.trim().toLowerCase() === "rock" || 
-    playerSelection.trim().toLowerCase() === "paper" || 
-    playerSelection.trim().toLowerCase() === "scissors") 
+    const formattedInput = playerSelection.trim().toLowerCase();
+    
+    if (formattedInput === "rock" || formattedInput === "paper" || formattedInput === "scissors") {
         return true;
-    else
+    } else {
+        console.error("Invalid input! Please, choose rock, paper, or scissors");
         return false;
+    }
 }
 
 function game() {
     console.log("Welcome to this rock, paper, scissors game! Write your choice in the prompt field");
+
     for (let i = 1; i <= 5; i++) {
-        let playerSelection = prompt(`Round ${i}: Choose Rock, Paper or Scissors`);
+        let playerSelection = prompt(`Round ${i}: Choose Rock, Paper, or Scissors`);
         let computerSelection = computerPlay();
+
         if (playerSelection === null) {
             gameAborted = true;
             console.log("Game aborted.");
             break;
-        }
-        else{
-
-        }
-        if (isInputValid(playerSelection))
-            console.log(playRound(playerSelection.trim().toLowerCase(), computerSelection));
-        else{
-            console.error("Invalid input! Please, choose rock, paper or scissors");
+        } else if (isInputValid(playerSelection)) {
+            const roundResult = playRound(playerSelection.trim().toLowerCase(), computerSelection);
+            console.log(roundResult);
+        } else {
+            console.error("Invalid input! Please, choose rock, paper, or scissors");
             i--;
         }
-    
-    
     }
-    if (!gameAborted){
+
+    if (!gameAborted) {
         if (playerWins > computerWins) {
             console.log(`Congratulations! You won ${playerWins} to ${computerWins}!`)
-        } else if (playerWins < computerWins){
+        } else if (playerWins < computerWins) {
             console.log(`You failed! The computer won ${computerWins} to ${playerWins}!`)
         } else {
             console.log(`It's a tie! Guess none of you was strong enough to defeat the other`)
